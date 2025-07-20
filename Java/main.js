957dec4a7748fe938ffaaf7b067b2ca0e2c04ab3
Hieulaptrinh <trunghieuk09645@gmail.com>
2025-07-20 10:34:29 +0700

    Header_web

---
+++ Java/main.js
@@ -0,0 +1,56 @@
+function passwordValid(password) {
+  if (password.length < 6) {
+    return false;
+  }
+  let DownCase = false;
+  let UpCase = false;
+  let Digit = false;
+
+  for (let i = 0; i < password.length; i++) {
+    let char = password[i];
+    if (char >= "a" && char <= "z") {
+      DownCase = true;
+    }
+    if (char >= "A" && char <= "Z") {
+      UpCase = true;
+    }
+    if (char >= "0" && char <= "9") {
+      Digit = true;
+    }
+  }
+  return DownCase && UpCase && Digit;
+}
+
+function setpassword() {
+  let password = prompt("Nhập mật khẩu muốn đặt:");
+  if (passwordValid(password)) {
+    alert(`Bạn đã đặt mật khẩu thành công`);
+    return password;
+  } else {
+    alert(`Mật khẩu không hợp lệ!
+      1.Có ít nhất 6 kí tự
+      2.Có ít nhất 1 số
+      3.Có ít nhất 1 kí tự viết hoa
+      4.Có ít nhất 1 kí tự viết thường`);
+    return setpassword();
+  }
+}
+
+let passwordOK = setpassword();
+
+let LoginCount = 0;
+while (true) {
+  let PassLogin = prompt(`Nhập mật khẩu để đăng nhập!`);
+  if (PassLogin === passwordOK) {
+    alert(`Bạn đã đăng nhập thành công!`);
+    break;
+  } else {
+    LoginCount++;
+    if (LoginCount < 5) {
+      alert(`Sai mật khẩu! Bạn còn ${5 - LoginCount} lần thử`);
+    } else {
+      alert(`Tài khoản của bạn đã bị khóa. Liên hệ Admin Trung Hiếu`);
+      break;
+    }
+  }
+}
